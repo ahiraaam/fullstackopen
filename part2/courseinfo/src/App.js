d842dfc23142845e97d48b411f5dfc84a2ab1346
ahiraaam <ahicruma23@gmail.com>
2022-06-29 20:14:12 -0500

    2.1-2.4

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,79 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) =>
+  parts.map((part) => {
+    return <Part key={part.id} part={part} />;
+  });
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={total} />
+    </div>
+  );
+};
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return courses.map((course) => {
+    return <Course course={course} />;
+  });
+};
+
+export default App;
